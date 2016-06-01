9569bf2acba7b32656b4d5bd95eb34c157524c74
Yakeracles <271352565@qq.com>
2016-06-01 11:33:32 +0800

    55
    sSD

--- gitHub/gitHub/ViewController.m
+++ gitHub/gitHub/ViewController.m
@@ -17,15 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    /**
+/    /**
      *  dsafasdfsgas
      *
      *  @return asdfsaasgda
      */
+<<<<<<< HEAD
     NSLog(@"----666");
 
      NSLog(@"----你好很大佛奥 放到放到撒 啊范德萨");
     
+=======
+>>>>>>> parent of 2748c27... yake
 }
 
 - (void)didReceiveMemoryWarning {
