2748c27226b208f9687971f45e078d2f053c37bc
Yakeracles <271352565@qq.com>
2016-06-01 09:46:18 +0800

    yake
    2222

--- gitHub/gitHub/ViewController.m
+++ gitHub/gitHub/ViewController.m
@@ -17,11 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-/    /**
+    /**
      *  dsafasdfsgas
      *
      *  @return asdfsaasgda
      */
+    NSLog(@"----666");
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
