0290242ef910a3250396db14ff8b8302140a9567
Yakeracles <271352565@qq.com>
2016-06-01 09:34:39 +0800

    yake
    add project

---
+++ gitHub/gitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitHub
+//
+//  Created by student on 16/6/1.
+//  Copyright © 2016年 Yake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitHub/gitHub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitHub
+//
+//  Created by student on 16/6/1.
+//  Copyright © 2016年 Yake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
